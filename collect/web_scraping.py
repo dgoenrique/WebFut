cec295fa2dbe194fcb033f852c9ba54e4988f0c5
dgoenrique <diego.enrique.silvalima@gmail.com>
2025-12-05 16:57:13 -0300

    feat: Initial codebase for the project - Data extraction and manipulation

---
+++ collect/web_scraping.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup 
+import cloudscraper
+import pandas as pd
+
+scrapper = cloudscraper.create_scraper()
+
+all_items_data = []
+
+# Teste para a página de camisas-retro
+
+for i in range(1,11):
+    url = f"https://www.recordsports.net/camisas-retro/page/{i}/"
+    resp = scrapper.get(url)
+
+    if resp.status_code == 200:
+        soup = BeautifulSoup(resp.text, 'html.parser')
+
+        item_link = soup.find_all('a', class_='item-link')
+
+        
+
+        for item_link in item_link:
+            item_name_tag = item_link.find('div', class_='item-name')
+            item_name = item_name_tag.text.strip()
+
+            item_price_tag = item_link.find('span', class_='item-price')
+            item_price = item_price_tag.text.strip()
+
+            all_items_data.append({
+                'nome': item_name,
+                'preço': item_price
+            })
+
+for item in all_items_data:
+    print(f"Nome: {item['nome']}, Preço: {item['preço']}")
+
+df = pd.DataFrame(all_items_data)
+df.to_csv('data.csv', index=False)
+
+
+
